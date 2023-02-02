caca031aa8d14ce0c3d16e8fe8883cba4d5ce498
lordmike007 <manujasan23@gmail.com>
2023-02-02 15:24:52 +0530

    added push notification support

--- contracts/SOUL.sol
+++ contracts/SOUL.sol
@@ -4,21 +4,36 @@ pragma solidity ^0.8.7;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./SOULNFT.sol";
 
-contract SOULTOKEN is ERC20 {
+interface IPUSHCommInterface {
+    function sendNotification(
+        address _channel,
+        address _recipient,
+        bytes calldata _identity
+    ) external;
+}
 
-    mapping (address=>uint) public strikes;
-    mapping (address=>uint) public rewardCount;
-    mapping (address=>mapping(uint=>uint8)) rewardTime;
+contract SOULTOKEN is ERC20 {
+    mapping(address => uint) public strikes;
+    mapping(address => uint) public rewardCount;
+    mapping(address => mapping(uint => uint8)) rewardTime;
     uint64 private referenceTime = 1673202600;
     uint24 private secondsInADay = 86400;
+    address EPNS__COMM = 0xb3971BCef2D791bc4027BbfedFb47319A4AAaaAa;
+    address CHANNEL_ADDRESS;
     SOULNFTS soulnft;
 
-    constructor(string memory _name, string memory _symbol, address soulNFTContract) ERC20(_name, _symbol) {
-        _mint(address(this), 100000 * 10^18);
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address soulNFTContract,
+        address channelAddress
+    ) ERC20(_name, _symbol) {
+        _mint(address(this), (100000 * 10) ^ 18);
         soulnft = SOULNFTS(soulNFTContract);
+        CHANNEL_ADDRESS = channelAddress;
     }
 
     function assignStrike(address recipient) public {
@@ -26,19 +41,24 @@ contract SOULTOKEN is ERC20 {
     }
 
     function rewardNFT(address recipient) public {
-        require(rewardCount[recipient] >= 100, "recipient not eligible for reward");
+        require(
+            rewardCount[recipient] >= 100,
+            "recipient not eligible for reward"
+        );
         soulnft.RewardASoul(recipient);
     }
 
-    function checkEligibilityForNFT(address recipient) view public returns(bool)  {
-        return (rewardCount[recipient] == 100)?true: false;
+    function checkEligibilityForNFT(
+        address recipient
+    ) public view returns (bool) {
+        return (rewardCount[recipient] == 100) ? true : false;
     }
 
-    function isNotBanned(address recipient) view public returns(bool) {
+    function isNotBanned(address recipient) public view returns (bool) {
         return strikes[recipient] < 5;
     }
 
-    modifier isEligibleToReward {
+    modifier isEligibleToReward() {
         require(isNotBanned(msg.sender), "Rewarder has too many strikes");
         _;
     }
@@ -49,19 +69,60 @@ contract SOULTOKEN is ERC20 {
 
         uint currentTime = block.timestamp;
         uint timepassed = currentTime - referenceTime;
-        uint daysPassed = timepassed/secondsInADay;
+        uint daysPassed = timepassed / secondsInADay;
         uint currentDay = referenceTime + daysPassed * secondsInADay;
 
-        require(rewardTime[msg.sender][currentDay] < 3, "Rewarder Exceeding rewarding limit today.");
-        
+        require(
+            rewardTime[msg.sender][currentDay] < 3,
+            "Rewarder Exceeding rewarding limit today."
+        );
+
         rewardCount[recipient]++;
         rewardTime[msg.sender][currentDay]++;
 
-        if(checkEligibilityForNFT(recipient)){
+        if (checkEligibilityForNFT(recipient)) {
             rewardNFT(recipient);
         }
 
-        _transfer(address(this),recipient,1 * 10^18);
-    }
+        _transfer(address(this), recipient, (1 * 10) ^ 18);
 
+        IPUSHCommInterface(EPNS__COMM)
+            .sendNotification(
+                CHANNEL_ADDRESS, // from channel - recommended to set channel via dApp and put it's value -> then once contract is deployed, go back and add the contract address as delegate for your channel
+                recipient, // to recipient, put address(this) in case you want Broadcast or Subset. For Targetted put the address to which you want to send
+                bytes(
+                    string(
+                        // We are passing identity here: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/identity/payload-identity-implementations
+                        abi.encodePacked(
+                            "0", // this is notification identity: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/identity/payload-identity-implementations
+                            "+", // segregator
+                            "3", // this is payload type: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/payload (1, 3 or 4) = (Broadcast, targetted or subset)
+                            "+", // segregator
+                            "New Reward!", // this is notificaiton title
+                            "+", // segregator
+                            "You were rewarded a SOUL Token" // notification body
+                        )
+                    )
+                )
+            );
+        IPUSHCommInterface(EPNS__COMM)
+            .sendNotification(
+                CHANNEL_ADDRESS, // from channel - recommended to set channel via dApp and put it's value -> then once contract is deployed, go back and add the contract address as delegate for your channel
+                msg.sender, // to recipient, put address(this) in case you want Broadcast or Subset. For Targetted put the address to which you want to send
+                bytes(
+                    string(
+                        // We are passing identity here: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/identity/payload-identity-implementations
+                        abi.encodePacked(
+                            "0", // this is notification identity: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/identity/payload-identity-implementations
+                            "+", // segregator
+                            "3", // this is payload type: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/payload (1, 3 or 4) = (Broadcast, targetted or subset)
+                            "+", // segregator
+                            "Reward Sent", // this is notificaiton title
+                            "+", // segregator
+                            "Your reward was sent to the recipient" // notification body
+                        )
+                    )
+                )
+            );
+    }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,6 +7,7 @@
 const hre = require("hardhat");
 
 async function main() {
+  const pushContractAddress = '0xEFDeb468C681EEF5Df33E58920B3027B52d525D7';
 
   const SoulNFTContractFactory = await hre.ethers.getContractFactory("SOULNFTS");
   const soulNFTContract = await SoulNFTContractFactory.deploy("SOULNFT", "SFT");
@@ -14,11 +15,11 @@ async function main() {
   await soulNFTContract.deployTransaction.wait();
 
   const SOULTOKENContractFactory = await hre.ethers.getContractFactory("SOULTOKEN");
-  const soulTokenContract = await SOULTOKENContractFactory.deploy("SOULTKN", "SOUL",soulNFTContract.address);
+  const soulTokenContract = await SOULTOKENContractFactory.deploy("SOULTKN", "SOUL",soulNFTContract.address, pushContractAddress);
   console.log("address: ", soulTokenContract.address)
   await soulTokenContract.deployTransaction.wait();
-  // address:  0x94575136082bE3E2A0fbC7DdD810afaD3702Dd73
-  // address:  0x0b4c74019c91cC3aC7EEa02a5C82B59917DE2a85
+  // address:  0x5FbDB2315678afecb367f032d93F642f64180aa3
+  // address:  0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
 }
 
 // We recommend this pattern to be able to use async/await everywhere
